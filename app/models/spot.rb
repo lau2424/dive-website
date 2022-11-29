77d30333aabda281ab0ae8b755ed3be75fc139ad
laurent larfa <laurent.larfa@gmail.com>
2022-11-29 14:28:09 +0100

    J2 all views and forms (restricted area done)

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,10 +1,11 @@
 class Spot < ApplicationRecord
   belongs_to :user
+  has_many :reviews
 
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
   # has many reviews ?
-  
+
 end
