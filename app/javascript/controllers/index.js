baccd29f7bf0447fab7f5ad6fefbb1cb839cb155
Fabrice Pestel <fhd.pestel@gmail.com>
2022-12-06 15:33:55 +0100

    star rating working and temperatures changed to integers

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -13,6 +13,9 @@ application.register("hello", HelloController)
 import MapController from "./map_controller"
 application.register("map", MapController)
 
+import StarRatingController from "./star_rating_controller"
+application.register("star-rating", StarRatingController)
+
 import WeatherController from "./weather_controller"
 application.register("weather", WeatherController)
 
